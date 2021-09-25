d3dde3fe7c01063d8ac554264893c46ccaf5450f
Anonymousloger <jamesnikhil22@gmail.com>
2021-09-25 10:56:25 +0530

    college project

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+// DB parameters
+define('DB_HOST', 'udemy-mysql');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'shareposts');
+
+// App root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL root
+define('URLROOT', 'http://localhost:8082/shareposts');
+
+// Site name
+define('SITENAME', 'SharePosts');
+
+// App version
+define('APPVERSION', '1.0.0');
